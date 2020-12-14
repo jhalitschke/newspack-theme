66dd6bdd7ad12623833b65e2c641e4c770f29b0d
Joerg Halitschke <j.halitschke@funkedigital.de>
2020-12-14 09:38:38 +0100

    added newspack-wmn

---
+++ newspack-wmn/template-parts/ads/outbrain.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template part for displaying Outbrain Article Widget
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Newspack
+ */
+
+?>
+
+<div class="OUTBRAIN"
+	 data-src="<?php echo get_permalink(); ?>"
+	 data-widget-id="AR_1"></div>
+<script type="text/javascript" async="async" src="//widgets.outbrain.com/outbrain.js"></script>
+
